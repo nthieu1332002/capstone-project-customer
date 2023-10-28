d1ae89415b7fe45ea0d5a780136543a48afdc846
Trung Hieu <nthieu1332002@gmail.com>
2023-10-28 15:40:52 +0700

    test fix error

---
+++ app/booking/layout.tsx
@@ -0,0 +1,5 @@
+const BookingIdLayout = async ({ children }: { children: React.ReactNode }) => {
+  return <div className="min-h-screen pt-[68px]">{children}</div>;
+};
+
+export default BookingIdLayout;
