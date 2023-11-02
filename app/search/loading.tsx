20db8b77669142ab1ac30454d1251f4b7aeef8c9
Trung Hieu <nthieu1332002@gmail.com>
2023-11-02 22:11:14 +0700

    [UI] Add loading skeleton search page

---
+++ app/search/loading.tsx
@@ -0,0 +1,16 @@
+import { Divider } from "antd";
+
+export default function Loading() {
+  return (
+    <div className="animate-pulse py-8 mx-auto px-4 md:px-8 lg:px-16">
+      <div className="h-10 bg-slate-200 rounded mb-5"></div>
+
+      <div className="flex flex-col gap-5">
+        <div className="h-40 bg-slate-200 rounded"></div>
+        <div className="h-40 bg-slate-200 rounded"></div>
+        <div className="h-40 bg-slate-200 rounded"></div>
+        <div className="h-40 bg-slate-200 rounded"></div>
+      </div>
+    </div>
+  );
+}
