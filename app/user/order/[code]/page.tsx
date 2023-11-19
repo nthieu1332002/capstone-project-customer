fa2d97624f02b115f38ec0ab7dc2e2b38822db06
Trung Hieu <nthieu1332002@gmail.com>
2023-11-19 21:04:29 +0700

    [PAYMENT] Account customer create payment - done

--- app/user/order/[code]/page.tsx
+++ app/user/order/[code]/page.tsx
@@ -1,4 +1,5 @@
 import getServerUser from "@/actions/getServerUser";
+import BookingStatus from "@/components/bookings/BookingStatus";
 import OrderDetailBody from "@/components/users/orders/OrderDetailBody";
 import OrderDetailHeader from "@/components/users/orders/OrderDetailHeader";
 import { axios } from "@/lib/axios";
@@ -7,6 +8,17 @@ import { redirect } from "next/navigation";
 const getOrderDetail = async (code: string) => {
   try {
     const res = await axios.get(`/api/customer/orders/${code}`);
+
+    return res.data;
+  } catch (error) {
+    console.log(error);
+    throw new Error("failed to get order detail");
+  }
+};
+const getOrderTracking = async (code: string) => {
+  try {
+    const res = await axios.get(`/api/customer/orders/tracking/${code}`);
+
     return res.data;
   } catch (error) {
     console.log(error);
@@ -18,19 +30,32 @@ type Props = {
   params: {
     code: string;
   };
+  searchParams?: { [key: string]: string | undefined };
 };
 
-const OrderDetail = async ({ params }: Props) => {
+const OrderDetail = async ({ params, searchParams }: Props) => {
   const profile = await getServerUser();
+  const { vnp_TransactionStatus } = searchParams || {};
+
   if (!profile) redirect("error");
-  const order = await getOrderDetail(params.code);
-  console.log("order", order);
+  const [order, tracking] = await Promise.all([
+    getOrderDetail(params.code),
+    getOrderTracking(params.code),
+  ]);
+  order.attributes.status_history = tracking.details;
   if (!order) redirect("error");
   const data = order.attributes;
+  console.log("data", data);
   return (
     <div className="p-3 flex flex-col gap-4">
+      {vnp_TransactionStatus === "00" ? (
+        <BookingStatus
+          content="Thanh toán thành công"
+          status="success"
+        />
+      ) : null}
       <OrderDetailHeader code={params.code} order={data} />
-      <OrderDetailBody order={data} />
+      <OrderDetailBody code={params.code} order={data} />
     </div>
   );
 };
