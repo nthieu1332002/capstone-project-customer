633c5e5596d5af93aa7fdfd018d755ad8fbb87c4
Trung Hieu <nthieu1332002@gmail.com>
2023-11-28 16:16:12 +0700

    [TRACKING] Fix tracking data

--- app/tracking/page.tsx
+++ app/tracking/page.tsx
@@ -5,7 +5,7 @@ import { cache } from "react";
 
 const getSearchList = cache(async ({ code }: { code: string }) => {
   try {
-    const res = await axios.get(`/api/customer/orders/tracking/${code}`);
+    const res = await axios.get(`/api/customer/orders/${code}/tracking/`);
     console.log("res", res.data);
     return res.data;
   } catch (error) {
@@ -20,29 +20,6 @@ export default async function Search({
 }) {
   const code = searchParams?.code;
   const data = code ? await getSearchList({ code }) : {};
-  // const data = {
-  //   details: [
-  //     {
-  //       status: 0,
-  //       created_at: "2023-11-06T04:34:48.000000Z",
-  //       location: "Hà Nội",
-  //     },
-  //     {
-  //       status: 1,
-  //       created_at: "2023-11-08T05:34:48.000000Z",
-  //       location: "Hà Nội",
-  //     },
-  //     {
-  //       status: 2,
-  //       created_at: "2023-11-16T06:34:48.000000Z",
-  //       location: "Hà Nội",
-  //     },
-  //   ],
-  //   payment: {
-  //     status: 0,
-  //     value: 0,
-  //   },
-  // };
   console.log("dât", data);
   return (
     <div className="min-h-screen pt-[68px] bg-slate-50">
--- components/trackings/TrackingContent.tsx
+++ components/trackings/TrackingContent.tsx
@@ -6,13 +6,12 @@ import { cn } from "@/lib/utils";
 import { OrderStatusMap } from "@/lib/constants";
 dayjs.locale("vi");
 
-
-
 export type Tracking = {
-  details: Array<{
-    status: number;
-    created_at: string;
+  milestones: Array<{
+    type: number;
+    achievedAt: string;
     location: string | null;
+    address: string | null;
   }>;
   payment: {
     status: number;
@@ -26,45 +25,44 @@ type Props = {
 };
 
 const TrackingContent = ({ data, code }: Props) => {
-  const lastSegment = data.details[data.details.length - 1];
-  const item = data.details.map((item) => {
-    const orderStatus = OrderStatusMap[item.status];
+  const lastSegment = data.milestones[data.milestones.length - 1];
+  const item = data.milestones.map((item, index) => {
+    const orderStatus = OrderStatusMap[item.type];
     return {
-
-      label: (
-        <div className="text-center rounded-sm py-5">
-          <p className="font-medium">
-            {dayjs(item.created_at).format("DD/MM/YYYY")}
-          </p>
-          <p className="">{dayjs(item.created_at).format("hh:mm")}</p>
-        </div>
-      ),
       children: (
-        <div className="bg-white p-5 rounded-md min-h-[89px]">
-          <p className="font-semibold">Đơn hàng {orderStatus}</p>
-          <p className="text-gray-500">{item.location}</p>
+        <div key={index}>
+          <p className="uppercase font-medium text-base text-gray-600 mb-1">
+            Ngày {dayjs(item.achievedAt).format("DD/MM/YYYY lúc hh:mm:ss")}
+          </p>
+          <div className="bg-white p-3 py-2 text-lg rounded-md min-h-[70px] shadow-sm">
+            <b className="font-semibold">Đơn hàng {orderStatus} {item.location ? (`tại ${item.location}`) : null} </b>
+            <p className="text-gray-500">{item.address}</p>
+          </div>
         </div>
       ),
     };
   });
   return (
-    <div className="text-center">
-      <h1 className="font-bold text-xl">Đơn hàng {code}</h1>
-      <p>
-        Đơn hàng{" "}
-        <b
-          className={cn(
-            lastSegment.status === 5 ? "text-red-600" : "text-primary-color"
-          )}
-        >
-          {OrderStatusMap[lastSegment.status]}
-        </b>{" "}
-        vào lúc {dayjs(lastSegment.created_at).format("hh:mm dddd DD/MM/YYYY")}
-      </p>
-      <div className="timeline_container mt-8 px-3 lg:px-40">
-        <Timeline className="timeline_container" mode="left" reverse items={item} />
+    <>
+      <div className="text-center">
+        <h1 className="font-bold text-xl">Đơn hàng {code}</h1>
+        <p>
+          Đơn hàng{" "}
+          <b
+            className={cn(
+              lastSegment.type === 6 ? "text-red-600" : "text-primary-color"
+            )}
+          >
+            {OrderStatusMap[lastSegment.type]}
+          </b>{" "}
+          vào lúc{" "}
+          {dayjs(lastSegment.achievedAt).format("hh:mm dddd DD/MM/YYYY")}
+        </p>
       </div>
-    </div>
+      <div className="mt-8">
+        <Timeline reverse items={item} />
+      </div>
+    </>
   );
 };
 
